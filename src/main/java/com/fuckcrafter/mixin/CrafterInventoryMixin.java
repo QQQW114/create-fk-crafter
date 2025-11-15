3fbc8f8ee564df62fc6c97b25bdff40edc4c0391
QQQQW114 <qwrt114@163.con>
2025-11-15 23:32:27 +0800

    1

--- src/main/java/com/fuckcrafter/mixin/CrafterInventoryMixin.java
+++ src/main/java/com/fuckcrafter/mixin/CrafterInventoryMixin.java
@@ -1,24 +1,15 @@
 package com.fuckcrafter.mixin;
 
 import com.simibubi.create.content.kinetics.crafter.MechanicalCrafterBlockEntity.Inventory;
-import com.simibubi.create.foundation.item.SmartInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Inventory.class, remap = false)
 public class CrafterInventoryMixin {
-
-    @Redirect(
-            method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lcom/simibubi/create/foundation/item/SmartInventory;forbidExtraction()V",
-                    remap = false
-            ),
-            remap = false
-    )
-    private void skipForbidExtraction(SmartInventory instance) {
-        System.out.println("[FuckCrafter] Mixin applied! Extraction allowed.");
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void allowExtraction(CallbackInfo ci) {
+        ((Inventory)(Object)this).allowExtraction();
     }
 }
